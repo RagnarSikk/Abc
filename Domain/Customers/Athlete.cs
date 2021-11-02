a4a2985e7fd0064c8423d67d78c9e2d8be3e3594
Ralf-Stiven Viru <raviru@ttu.ee>
2021-11-02 11:40:38 +0200

    Domain MyShopi jargi- Customers lisatud

---
+++ Domain/Customers/Athlete.cs
@@ -0,0 +1,20 @@
+﻿using Abc.Data.Customers;
+using Abc.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abc.Domain.Customers
+{
+    public sealed class Athlete : UniqueEntity<AthleteData>
+    {
+        public Athlete(AthleteData d) : base(d) { }
+        public double Weight => Data?.Weight ?? UnspecifiedDouble;
+        public double Height => Data?.Height ?? UnspecifiedDouble;
+        public int AmountOfVisits => Data?.AmountOfVisits ?? UnspecifiedInteger;
+
+    }
+}
+ 
