b1e3bad54ce13c487fda995cb94cf08968489ec0
Ragnar Sikk <ragsik@ttu.ee>
2021-11-02 11:15:57 +0200

    Fixed errors - Description > Definition

---
+++ Tests/Domain/IsDomainTested.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Abc.Tests.Domain
+{
+    [TestClass] public class IsDomainTested : AssemblyTests {
+        protected override string assembly => "Abc.Data";
+        [TestMethod]
+        public void IsCommonTested()
+            => isAllTested(assembly, nameSpace("Common"));
+        [TestMethod]
+        public void IsShopTested()
+            => isAllTested(assembly, nameSpace("Shop"));
+    }
+}
---
+++ Tests/TestAssertions.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Abc.Tests {
+    public class TestAssertions {
+        protected static void notTested(string message = null) => Assert.Inconclusive(message);
+        protected static void notTested(string message, params object[] parameters)
+            => Assert.Inconclusive(message, parameters);
+        protected static void areEqual<TExpected, TActual>(TExpected e, TActual a) => Assert.AreEqual(e, a);
+    }
+}
