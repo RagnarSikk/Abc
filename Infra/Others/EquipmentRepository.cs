1d6db4480ef128a8303d13764d6094a0bb0459af
Kristina Mere <krmere@ttu.ee>
2021-11-02 21:12:11 +0200

    Infra Repositoryd

---
+++ Infra/Others/EquipmentRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Abc.Data.Others;
+using Abc.Domain.Others;
+using Abc.Infra.Common;
+
+namespace Abc.Infra.Others
+{
+    public sealed class EquipmentRepository : UniqueEntitiesRepository<Equipment, EquipmentData>, IEquipmentRepository
+    {
+        public EquipmentRepository(TrainingDbContext c) : base(c, c.Equipments) { }
+        protected internal override Equipment toDomainObject(EquipmentData d) => new Equipment(d);
+
+    }
+}
