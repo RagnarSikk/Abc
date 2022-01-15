588451e04b5abc6bb502119b54cb664d3af6ef01
Ralf-Stiven Viru <raviru@ttu.ee>
2022-01-16 00:13:18 +0200

    Factory testid

--- Tests/Facade/Others/Factories/BrandViewFactoryTests.cs
+++ Tests/Facade/Others/Factories/BrandViewFactoryTests.cs
@@ -1,10 +1,16 @@
 ﻿using Abc.Data.Others;
 using Abc.Domain.Others;
-using Abc.Facade.Common;
 using Abc.Facade.Others;
 using Abc.Facade.Others.Factories;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace Abc.Tests.Facade.Others.Factories {
-  
+namespace Abc.Tests.Facade.Others.Factories
+{
+    [TestClass]
+    public class BrandViewFactoryTests : FactoryBaseTests<BrandViewFactory, BrandData, Brand, BrandView>
+    {
+        protected override Brand createObject(BrandData d) => new(d);
+
+       
+    }
 }
