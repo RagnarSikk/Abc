ceab11366615b514a685681c58c6e73dd1c450d9
Kristina Mere <krmere@ttu.ee>
2022-01-15 13:06:32 +0200

    baseEntitytest

---
+++ Tests/Domain/Common/BaseEntityTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Abc.Aids.Random;
+using Abc.Domain.Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Abc.Tests.Domain.Common
+{
+    [TestClass] public class BaseEntityTests : AbstractTests<object> {
+        private class testClass : BaseEntity { }
+        protected override object createObject() => new testClass();
+        [TestMethod] public void UnspecifiedTest() => isProperty(Aids.Constants.Word.Unspecified);
+        [TestMethod] public void UnspecifiedValidFromTest() => isProperty(DateTime.MinValue);
+        [TestMethod] public void UnspecifiedValidToTest() => isProperty(DateTime.MaxValue);
+        [TestMethod] public void UnspecifiedDoubleTest() => isProperty(double.NaN);
+        [TestMethod] public void UnspecifiedDecimalTest() => isProperty(decimal.MaxValue);
+        [TestMethod] public void UnspecifiedIntegerTest() => isProperty(0);
+    }
+
+}
